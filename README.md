f5d65a8d17d0e93fed31d6756dc6506155cec64c
Benjamin Hudgens <benjamin@benjamindavid.com>
2018-09-04 03:22:29 -0500

    ...in the beginning

---
+++ README.md
@@ -0,0 +1,5 @@
+#### Install with curl
+
+```shell
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/bhudgens/benvironment/master/install.sh)"
+```
---
+++ install.sh
@@ -0,0 +1,15 @@
+main() {
+  URL_PATH_TO_ENV_FILES="https://raw.githubusercontent.com/bhudgens/benvironment/master"
+  FILE=".benvironment"
+  curl -s "${URL_PATH_TO_ENV_FILES}/${FILE}" -o "${HOME}/${FILE}"
+  ENVS=".zshrc .bashrc"
+  for env_file in $(echo $ENVS); do
+    if ! grep "${FILE}" "${HOME}/${env_file}" > /dev/null; then
+      echo Installing benvironment into ${env_file}
+      echo "source ${FILE}" >> "${HOME}/${env_file}"
+    fi
+  done
+  echo All Done!
+}
+
+main
