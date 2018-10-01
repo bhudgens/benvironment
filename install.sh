f1a188a3283cf8ab73bfc25176e66a7febd6c78d
Benjamin Hudgens <benjamin@benjamindavid.com>
2018-10-01 11:07:16 -0500

    Don't install for bash

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ main() {
   URL_PATH_TO_ENV_FILES="https://raw.githubusercontent.com/bhudgens/benvironment/master"
   FILE=".benvironment"
   curl -s "${URL_PATH_TO_ENV_FILES}/${FILE}" -o "${HOME}/${FILE}"
-  ENVS=".zshrc .bashrc"
+  ENVS=".zshrc"
   for env_file in $(echo $ENVS); do
     if ! grep "${FILE}" "${HOME}/${env_file}" > /dev/null; then
       echo Installing benvironment into ${env_file}
