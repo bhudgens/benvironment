748173da2822f86a5b26bc2d380d2360666cddb6
Benjamin Hudgens <benjamin@benjamindavid.com>
2023-08-24 00:17:34 -0500

    Get rid of check
    - We only install this way on a new machine

--- install.sh
+++ install.sh
@@ -11,43 +11,36 @@ function _commandExists() {
 
 main() {
   BENVIRONMENT_LOAD_FILE=".benvironment"
-  for env_file in .zshrc; do
-    if ! grep "${BENVIRONMENT_LOAD_FILE}" "${HOME}/${env_file}" 2>&1 > /dev/null; then
-      # GITHUB_USERNAME=$(sh -c 'ssh -o "IdentitiesOnly=yes" git@github.com 2>&1 | grep "successfully authenticated" | cut -f2 -d" " | cut -f1 -d"!"')
-      # [ -z "$GITHUB_USERNAME" ] && _punt "You need to setup your key with github"
+  if _osType linux \
+    && _commandExists apt-get; then
 
-      if _osType linux \
-      && _commandExists apt-get; then
-
-        for _command in curl zsh git; do
-          if ! _commandExists _command; then
-            APPS_TO_INSTALL="$APPS_TO_INSTALL $_command"
-          fi
-        done
-
-        if [ -n "$APPS_TO_INSTALL" ]; then
-          [ "$EUID" -ne 0 ] && SUDO="sudo "
-          eval "$SUDO apt-get update -y"
-          eval "$SUDO apt-get install -y $APPS_TO_INSTALL"
-        fi
-
-      fi
-
-      if [ ! -d "$HOME/.oh-my-zsh" ]; then
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-      fi
-
-      URL_PATH_TO_ENV_FILES="https://raw.githubusercontent.com/bhudgens/benvironment/master"
-      if [ -f "./${BENVIRONMENT_LOAD_FILE}" ]; then
-        cp "./${BENVIRONMENT_LOAD_FILE}" "${HOME}/${BENVIRONMENT_LOAD_FILE}"
-      else
-        curl -s "${URL_PATH_TO_ENV_FILES}/${BENVIRONMENT_LOAD_FILE}" -o "${HOME}/${BENVIRONMENT_LOAD_FILE}"
-      fi
-
-      echo Installing benvironment into ${env_file}
-      echo "source ${BENVIRONMENT_LOAD_FILE}" >> "${HOME}/${env_file}"
+  for _command in curl zsh git; do
+    if ! _commandExists _command; then
+      APPS_TO_INSTALL="$APPS_TO_INSTALL $_command"
     fi
   done
+
+  if [ -n "$APPS_TO_INSTALL" ]; then
+    [ "$EUID" -ne 0 ] && SUDO="sudo "
+    eval "$SUDO apt-get update -y"
+    eval "$SUDO apt-get install -y $APPS_TO_INSTALL"
+  fi
+
+  fi
+
+  if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+  fi
+
+  URL_PATH_TO_ENV_FILES="https://raw.githubusercontent.com/bhudgens/benvironment/master"
+  if [ -f "./${BENVIRONMENT_LOAD_FILE}" ]; then
+    cp "./${BENVIRONMENT_LOAD_FILE}" "${HOME}/${BENVIRONMENT_LOAD_FILE}"
+  else
+    curl -s "${URL_PATH_TO_ENV_FILES}/${BENVIRONMENT_LOAD_FILE}" -o "${HOME}/${BENVIRONMENT_LOAD_FILE}"
+  fi
+
+  echo Installing benvironment into ${env_file}
+  echo "source ${BENVIRONMENT_LOAD_FILE}" >> "${HOME}/${env_file}"
   echo All Done! Open a new window!
 }
 
